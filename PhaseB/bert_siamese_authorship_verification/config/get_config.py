bae97b4bff781dd46c295c78b72b65b6b77a9708
Adir111 <118291963+Adir111@users.noreply.github.com>
2025-03-24 21:29:36 +0200

    redundant removed

--- PhaseB/bert_siamese_authorship_verification/config/get_config.py
+++ PhaseB/bert_siamese_authorship_verification/config/get_config.py
@@ -4,9 +4,7 @@ import yaml
 
 def get_config():
     # Determine correct path for config.yaml
-    config_path = os.path.join(os.path.dirname(__file__), "../config/config.yaml")
-    if not os.path.exists(config_path):
-        config_path = os.path.join(os.path.dirname(__file__), "config/config.yaml")
+    config_path = os.path.join(os.path.dirname(__file__), "./config.yaml")
 
     # Load config
     with open(config_path, "r") as f:
