1b63ff48f58fad6915efbdad55b111e353aee249
Adir111 <adir987321@gmail.com>
2025-04-30 19:40:12 +0300

    wip

--- PhaseB/bert_siamese_authorship_verification/src_new/model.py
+++ PhaseB/bert_siamese_authorship_verification/src_new/model.py
@@ -1,39 +1,33 @@
-import io
 import tensorflow as tf
 from transformers import TFBertModel
-from contextlib import redirect_stdout
 
 from PhaseB.bert_siamese_authorship_verification.utilities.env_handler import is_tf_2_10
 
 if is_tf_2_10():
-    from keras import Sequential, Input, Model
+    from keras import Input, Model
     from keras.layers import Dense, Bidirectional, Dropout, LSTM, Lambda
     from keras.layers.convolutional import Conv1D, MaxPooling1D
 else:
-    from tensorflow.keras import Sequential, Input, Model
+    from tensorflow.keras import Input, Model
     from tensorflow.keras.layers import Conv1D, MaxPooling1D, Dense, Bidirectional, Dropout, LSTM, Lambda
 
 class SiameseBertModel:
-    def __init__(self, config, logger, model_name="Default"):
-        self.config = config
-        self.logger = logger
-        self.kernel_size = self.config['model']['cnn']['kernel_size']
-        self.bilstm_layers = self.config['model']['bilstm']['number_of_layers']
-        self.bilstm_output_units = int(self.bilstm_layers * 2)
-        self.bilstm_dropout = self.config['model']['bilstm']['dropout']
-        self.filters = self.config['model']['cnn']['filters']
-        self.pool_size = self.config['model']['cnn']['pool_size']
-        self.in_features = self.config['model']['fc']['in_features']
-        self.out_features = self.config['model']['fc']['out_features']
-        self.padding = self.config['model']['cnn']['padding']
-        self.max_len = self.config['bert']['maximum_sequence_length']
+    def __init__(self, config, logger):
+        self.logger = logger # TODO: not used, should use?
+        self.kernel_size = config['model']['cnn']['kernel_size']
+        bilstm_layers = config['model']['bilstm']['number_of_layers']
+        self.bilstm_output_units = int(bilstm_layers * 2)
+        self.bilstm_dropout = config['model']['bilstm']['dropout']
+        self.filters = config['model']['cnn']['filters']
+        self.pool_size = config['model']['cnn']['pool_size']
+        self.in_features = config['model']['fc']['in_features']
+        self.out_features = config['model']['fc']['out_features']
+        self.padding = config['model']['cnn']['padding']
+        self.max_len = config['bert']['maximum_sequence_length']
         self.hidden_size = config['training']['hidden_size']
 
-        self.bert_model = TFBertModel.from_pretrained(self.config['bert']['model'])
-        self.bert_model.trainable = self.config['bert']['trainable']
-
-        self._branch = None
-        self._model_name = model_name
+        self.bert_model = TFBertModel.from_pretrained(config['bert']['model'])
+        self.bert_model.trainable = config['bert']['trainable']
 
     def __get_cnn_bilstm_stack(self):
         inputs = Input(shape=(self.max_len, self.hidden_size))
@@ -52,31 +46,49 @@ class SiameseBertModel:
         # Output Layer
         outputs = Dense(self.out_features, activation='relu')(x)
         model = Model(inputs=inputs, outputs=outputs, name="cnn_bilstm_stack")
-
         return model
 
-    def build_complete_model(self):
+    def __build_siamese_branch(self):
+        input_ids = Input(shape=(self.max_len,), dtype=tf.int32, name="input_ids")
+        attention_mask = Input(shape=(self.max_len,), dtype=tf.int32, name="attention_mask")
+
+        # BERT output
+        bert_output = self.bert_model(input_ids, attention_mask=attention_mask)[0]
+        print(f"BERT output shape: {bert_output.shape}")  # Debug print
+
+        # CNN BiLSTM output
+        cnn_bilstm_stack = self.__get_cnn_bilstm_stack()
+        output = cnn_bilstm_stack(bert_output)
+        print(f"Output after CNN+BiLSTM: {output.shape}")  # Debug print
+
+        return Model(inputs=[input_ids, attention_mask], outputs=output, name="siamese_branch")
+
+
+    def build_model(self):
         """
-        Build the full model with BERT preprocessing + CNN + BiLSTM layers.
+        Build the full model with BERT + CNN + BiLSTM layers.
         """
-        # Define the input for the BERT preprocessing
-        input_ids_1 = Input(shape=(self.max_len,), dtype=tf.int32, name="input_text1")
-        attention_mask_1 = Input(shape=(self.max_len,), dtype=tf.int32, name="attention_mask1")
-        input_ids_2 = Input(shape=(self.max_len,), dtype=tf.int32, name="input_text2")
-        attention_mask_2 = Input(shape=(self.max_len,), dtype=tf.int32, name="attention_mask2")
+        # Shared branch
+        shared_branch = self.__build_siamese_branch()
+
+        # Define the pair inputs for BERT
+        input_ids_1 = Input(shape=(self.max_len,), dtype=tf.int32, name="input_text_1")
+        attention_mask_1 = Input(shape=(self.max_len,), dtype=tf.int32, name="attention_mask_1")
+        input_ids_2 = Input(shape=(self.max_len,), dtype=tf.int32, name="input_text_2")
+        attention_mask_2 = Input(shape=(self.max_len,), dtype=tf.int32, name="attention_mask_2")
 
         # Get BERT embeddings for both inputs
-        bert_output_1 = self.bert_model(input_ids=input_ids_1, attention_mask=attention_mask_1)[1]  # shape: (None, 768)
-        bert_output_2 = self.bert_model(input_ids=input_ids_2, attention_mask=attention_mask_2)[1]  # shape: (None, 768)
+        bert_output_1 = shared_branch([input_ids_1, attention_mask_1])
+        bert_output_2 = shared_branch([input_ids_2, attention_mask_2])
 
-        # Shared CNN + BiLSTM branch
-        cnn_bilstm_model = self.__get_cnn_bilstm_stack()
-        out1 = cnn_bilstm_model(bert_output_1)
-        out2 = cnn_bilstm_model(bert_output_2)
+        # Distance computation
+        distance = Lambda(
+            lambda tensors: tf.sqrt(tf.reduce_sum(tf.square(tensors[0] - tensors[1]), axis=1, keepdims=True) + 1e-6)
+        )([bert_output_1, bert_output_2])
 
-        # Compute Euclidean Distance
-        distance = Lambda(lambda tensors: tf.sqrt(tf.reduce_sum(tf.square(tensors[0] - tensors[1]), axis=1, keepdims=True)))([out1, out2])
+        # Output will be in [0, 1], suitable for BCE
+        output = Dense(1, activation="sigmoid")(distance)
 
         # Final Model
-        model = Model(inputs=[input_ids_1, attention_mask_1, input_ids_2, attention_mask_2], outputs=distance)
+        model = Model(inputs=[input_ids_1, attention_mask_1, input_ids_2, attention_mask_2], outputs=output, name="bert_siamese_with_cnn_bilstm")
         return model
