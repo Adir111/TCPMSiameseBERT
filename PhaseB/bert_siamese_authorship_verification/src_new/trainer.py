029e77ae39b397c542f4aff48e7835cdc1df72ab
Adir111 <adir987321@gmail.com>
2025-04-30 17:59:50 +0300

    Added trainer class

---
+++ PhaseB/bert_siamese_authorship_verification/src_new/trainer.py
@@ -0,0 +1,26 @@
+import tensorflow as tf
+
+class Trainer:
+    def __init__(self, config, model, batch_size):
+        self.model = model
+        self.batch_size = batch_size
+        self.epochs = config['training']['epochs']
+        self.loss=config['training']['loss']
+        self.learning_rate = config['training']['optimizer']['initial_learning_rate']
+
+    def compile_model(self):
+        """Compile the model with an optimizer, loss function, and metrics."""
+        self.model.compile(
+            optimizer=tf.keras.optimizers.Adam(learning_rate=self.learning_rate),
+            loss='binary_crossentropy',
+            metrics=['accuracy']
+        )
+
+    def train(self, train_dataset):
+        """Train the model on the dataset."""
+        self.compile_model()
+        history = self.model.fit(
+            train_dataset.batch(self.batch_size),
+            epochs=self.epochs
+        )
+        return history
