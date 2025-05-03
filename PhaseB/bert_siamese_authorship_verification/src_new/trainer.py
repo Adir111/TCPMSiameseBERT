b399aa87943caa447beb61cf0cf9b13745d71512
Adir111 <adir987321@gmail.com>
2025-05-03 15:04:49 +0300

    summary not supported

--- PhaseB/bert_siamese_authorship_verification/src_new/trainer.py
+++ PhaseB/bert_siamese_authorship_verification/src_new/trainer.py
@@ -12,11 +12,6 @@ class Trainer:
         self.loss = config['training']['loss']
         self.learning_rate = float(config['training']['optimizer']['initial_learning_rate'])
 
-    def get_model_summary_string(self):
-        with io.StringIO() as buf, redirect_stdout(buf):
-            self.model.summary()
-            return buf.getvalue()
-
     def __compile_model(self):
         """Compile the model with an optimizer, loss function, and metrics."""
         self.model.compile(
@@ -25,8 +20,6 @@ class Trainer:
             metrics=['accuracy']
         )
         self.logger.log(f"[INFO] Model compiled with loss: {self.loss}, learning rate: {self.learning_rate}")
-        summary = self.get_model_summary_string()
-        self.logger.log(summary)
 
     def train(self, x_train, y_train, x_test, y_test):
         """Train the model on the dataset."""
