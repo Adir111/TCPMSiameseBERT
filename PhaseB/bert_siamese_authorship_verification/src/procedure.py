10da5d1946dc4321cd311fa7ab2853d1138cf433
Adir111 <adir987321@gmail.com>
2025-05-30 16:15:29 +0300

    unused

--- PhaseB/bert_siamese_authorship_verification/src/procedure.py
+++ PhaseB/bert_siamese_authorship_verification/src/procedure.py
@@ -148,7 +148,6 @@ class Procedure:
             self.data_visualizer.display_signal_plot(signal, text_name, model_name)
 
     def run_training_procedure(self):
-        impostors_names = self.data_loader.get_impostors_name_list()
         impostor_pairs, starting_iteration = self.__get_pairs_info()
 
         self.logger.info(f"Batch size is {self.training_batch_size}")
